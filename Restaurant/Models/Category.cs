7baeade899a64c0b54217185ed7de92bf027ee29
Amal Al-Momani <amal.almomani.ext@Tahaluf.ae>
2023-10-03 17:02:52 +0300

    hi

--- Restaurant/Models/Category.cs
+++ Restaurant/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Restaurant.Models;
 
@@ -8,7 +9,8 @@ public partial class Category
     public decimal Id { get; set; }
 
     public string? Categoryname { get; set; }
-
+    [NotMapped]
+    public IFormFile? ImageFile { get; set; }
     public string? Imagepath { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
